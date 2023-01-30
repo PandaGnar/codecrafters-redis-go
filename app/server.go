e28adadd6047886dfed933b7707b487f35c2f2bf
Jefferson Chen <me@chenjefferson.com>
2023-01-29 22:13:12 -0800

    add binding port 6379

--- app/server.go
+++ app/server.go
@@ -2,25 +2,24 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"net"
+	"os"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+const PORT string = ":6379"
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+func main() {
+	l, err := net.Listen("tcp", PORT)
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+		return
+	}
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+		return
+	}
+	defer l.Close()
 }
